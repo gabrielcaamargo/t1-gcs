99e71b1511f2febb275b77b32cd93c496f274be5
Rafael Menchik Birmann <rafaelmbyt@gmail.com>
2025-04-29 16:23:03 -0300

    Criado método que designa um funcionário responsável por um equipamento e colocado no cadastro do equipamento

--- src/Equipamento.java
+++ src/Equipamento.java
@@ -4,14 +4,16 @@ public class Equipamento {
     private String nome, descricao, dataAquisicao;
     private double valorAquisicao;
     private TipoEquipamento tipoEquipamento;
+    private Funcionario funcionario;
 
-    public Equipamento(int id, String nome, String descricao, String dataAquisicao, double valorAquisicao, TipoEquipamento tipo) {
+    public Equipamento(int id, String nome, String descricao, String dataAquisicao, double valorAquisicao, TipoEquipamento tipo, Funcionario funcionario) {
         this.id = id;
         this.nome = nome;
         this.descricao = descricao;
         this.dataAquisicao = dataAquisicao;
         this.valorAquisicao = valorAquisicao;
         this.tipoEquipamento = tipo;
+        this.funcionario = funcionario;
     }
 
     public void editarDescricao(String novaDescricao) {
@@ -20,5 +22,13 @@ public class Equipamento {
 
     public int getId() {
         return id;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
     }
 }
